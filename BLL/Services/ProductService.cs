1a292e4cc475c5fefcf2c5c4979a943637879944
Mea Hrbenic <mhrbenic1@etf.unsa.ba>
2023-11-07 00:37:42 +0100

    Add project files.

---
+++ BLL/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using BLL.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ProductService: IProducstService
+    {
+
+        public ProductService() { }
+
+        public List<string> GetProducstList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Melem2.0/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using BLL.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Melem2._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProducstService _productService; 
+
+        public ProductsController(IProducstService producstService)
+        {
+            _productService = producstService;
+        }
+
+        [HttpGet("GetProducts")]
+        public ActionResult<List<string>> GetProducts()
+        {
+            return _productService.GetProducstList();
+        }
+
+    }
+}
